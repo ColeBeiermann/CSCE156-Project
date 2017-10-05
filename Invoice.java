23be239f935a3a13d845c4433297bc31e5b233fd
ColeBeiermann <31995657+ColeBeiermann@users.noreply.github.com>
2017-10-05 13:07:12 -0500

    Add files via upload

---
+++ Invoice.java
@@ -0,0 +1,11 @@
+
+public class Invoice {
+
+	private String invoiceCode;
+	private String customerCode;
+	private String personCode;
+	private String invoiceDate;
+	private Product product;
+	
+	
+}
