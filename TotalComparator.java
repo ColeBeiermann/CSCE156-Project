57c6d79bd33706677bcdddd72afbd418d6340564
ColeBeiermann <31995657+ColeBeiermann@users.noreply.github.com>
2017-11-17 10:53:08 -0600

    Add files via upload

--- TotalComparator.java
+++ TotalComparator.java
@@ -4,8 +4,26 @@ public class TotalComparator implements Comparator<Invoice>{
 
 	@Override
 	public int compare(Invoice inv1, Invoice inv2) {
-		int x = (inv1.getInvoiceCode()).compareTo(inv2.getInvoiceCode());
-		return x;
+		double Total1 = 0;
+		double Total2 = 0;
+		inv1.getProductList();
+		for (Product a: inv1.getProductList()) {
+			Total1 += a.getSubtotal();
+		}
+		for (Product a: inv2.getProductList()) {
+			Total2 += a.getSubtotal();
+		}
+		
+		double x = (Total1 - Total2);
+		if (x>0) {
+			return 1;
+		}
+		else if (x<0) {
+			return -1;
+		}
+		else {
+			return 0;
+		}
 	}
 
 }
